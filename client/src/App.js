f926d6355a2e89034d97e3ec2a10aaaae1006f3a
soobinin <isb9082@naver.com>
2022-05-10 20:32:00 +0900

    setting

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-              <Routes>
-          <Route path="/" element={<MainPage />} />
-          {/* <Route path="/*" element={<NotFound />} /> */}
-          <Route path="/about" element={<AboutPage />} />
-          <Route
-            path="/picture"
-            element={<PicturePage data="전달된 props" />}
-          />
-          <Route path="/*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }

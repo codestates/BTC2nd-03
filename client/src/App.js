7da756e72a409612658d9c3b9baefb6575b07315
djshinnn <tls980824@naver.com>
2022-05-11 21:10:21 +0900

    feat: mnemonic wallet UI 구현중

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import FirstPage from "./components/firstpage";
 import SecondPage from "./components/secondpage";
+import ThirdPage from "./components/thirdpage";
+import FourthPage from "./components/fourthpage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<FirstPage />} />
         <Route path="/secondpage" element={<SecondPage />} />
+        <Route path="/thirdpage" element={<ThirdPage />} />
+        <Route path="/fourthpage" element={<FourthPage />} />
       </Routes>
     </Router>
   );
